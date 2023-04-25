318a8fe50f34c20c8069cca56b58fb540fa55c78
basementlivin <epedersen13@gmail.com>
2023-04-25 13:22:51 -0400

    SPRING CLEANING, pt. I

--- assets/article.js
+++
@@ -1,7 +0,0 @@
-let paragraphs = document.querySelectorAll('.article-template__content p');
-
-paragraphs.forEach((p) => {
-    if( p.getElementsByTagName('img').length > 1) {
-        p.classList.add('images');
-    }
-});

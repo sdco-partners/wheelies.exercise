baab95c814ce4c8a6085e40954c615551890e883
basementlivin <epedersen13@gmail.com>
2023-04-26 17:03:35 -0400

    SPRING CLEANING, pt. II

--- src/js/modules/policy-styles.js
+++
@@ -1,6 +0,0 @@
-export function initPolicyStyles() {
-    let policyTitle = document.querySelector('.shopify-policy__title');
-    let splitTitle = new SplitText(policyTitle, {type: "words"});
-    let firstWord = splitTitle.words[0];
-    gsap.set(firstWord, { fontStyle: "italic" });
-}

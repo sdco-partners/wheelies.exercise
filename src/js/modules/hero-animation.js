04c33019c4cf659d2cb128fa75bf493b442c0584
basementlivin <epedersen13@gmail.com>
2023-05-08 08:40:09 -0400

    Begin nested animation

--- src/js/modules/hero-animation.js
+++ src/js/modules/hero-animation.js
@@ -1,12 +1,23 @@
 export function initHeroAnimation() {
 
     let hero = document.querySelector(".js-hero");
+    let header = document.querySelector(".js-header");
 
     if (hero) {
         let images = hero.querySelectorAll("img");
+        let tagline = hero.querySelector("h2");
+
+        gsap.set(header, { display: "none" });
         gsap.set(images, { display: "none" });
 
+        function fadeOut() {
+            let fO = gsap.timeline({ repeat: 0, delay: 0.5 });
+                fO.to(images, { opacity: 0, duration: 1.25, ease: "sine.out" });
+                fO.to(tagline, { opacity: 0, duration: 1.25, ease: "sine.out" });
+                return fO;
+        }
+
         let landingAnim = gsap.timeline({ repeat: 0, delay: 0 });
-        landingAnim.to(images, { display: "block", duration: 1, stagger: 1 });
+            landingAnim.to(images, { display: "block", duration: 0.85, stagger: 1, onComplete: fadeOut });
     }
 }

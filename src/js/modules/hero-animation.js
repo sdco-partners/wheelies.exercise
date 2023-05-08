06acb5f3d47be85e680803fb1bcb2efcb9ce6a8f
basementlivin <epedersen13@gmail.com>
2023-05-08 14:24:43 -0400

    Animation progress; update contact field type

--- src/js/modules/hero-animation.js
+++ src/js/modules/hero-animation.js
@@ -13,20 +13,50 @@ export function initHeroAnimation() {
         gsap.set(donuts, { display: "none" });
 
         function slideshow() {
+            let image = gsap.utils.toArray(images);
+            let allButLast = image.slice(0, -1);
+            
             let slide = gsap.timeline({ repeat: 0, delay: 0 });
-                slide.to(images, { display: "block", duration: 0.75, stagger: 1, });
-                return slide;
+                slide.to(images, { display: "block", duration: 0.5, stagger: 1, });
+                slide.to(allButLast, { display: "none" });
+                
+            return slide;
         }
 
         function fadeOut() {
-            let fade = gsap.timeline({ repeat: 0, delay: 0.5 });
-                fade.to(images, { opacity: 0, duration: 1.25, ease: "sine.out" });
-                fade.to(tagline, { opacity: 0, duration: 1.25, ease: "sine.out" }, "<");
-                return fade;
+            let fade = gsap.timeline({ repeat: 0, delay: 0.25 });
+                fade.to(images, { opacity: 0, duration: 1, ease: "power1.out" });
+                fade.to(tagline, { opacity: 0, duration: 1, ease: "power1.out" }, "<");
+                
+            return fade;
+        }
+
+        function donutDance() {
+            
+            let part1 = donuts.querySelector(".part-one");
+            let part2 = donuts.querySelector(".part-two");
+            let part3 = donuts.querySelector(".part-three");
+            let part4 = donuts.querySelector(".part-four");
+            let part5 = donuts.querySelector(".part-five");
+            
+            let logoParts = [part1, part2, part3, part4, part5];
+
+            gsap.set(donuts, { display: "block", opacity: 1});
+            gsap.set(logoParts, { opacity: 0});
+
+            let dance = gsap.timeline({ repeat: 0, delay: 0 });
+                dance.to(part1, { opacity: 1, duration: 0.75, ease: "power1.in" });
+                dance.to(part5, { opacity: 1, duration: 0, delay: 0.75 });
+                dance.to(part3, { opacity: 1, duration: 0, delay: 0.75 });
+                dance.to(part4, { opacity: 1, duration: 0, delay: 0.75 });
+                dance.to(part2, { opacity: 1, duration: 0, delay: 0.75 });
+
+            return dance;
         }
 
         let landingAnim = gsap.timeline();
             landingAnim.add(slideshow());
             landingAnim.add(fadeOut(), ">");
+            landingAnim.add(donutDance(), ">");
     }
 }
